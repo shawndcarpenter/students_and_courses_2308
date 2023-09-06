02bb4673e3daccde0061a4595fe6c692f9b7a4fb
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-09-06 09:27:11 -0600

    Complete iteration 1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
 class Student
-  
+  attr_reader :name, :age, :scores
+
+  def initialize(student)
+    @name = student[:name]
+    @age = student[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    @scores.sum(0.0) / @scores.size
+  end
 end
